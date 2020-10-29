e1a2fc0a629a9310a492f4fe63d07d5fd587318d
lijun.huang <huanglijun317@126.com>
2020-10-29 16:23:55 +0800

    博客工程初始化

---
+++ app/route/index.js
@@ -0,0 +1,5 @@
+const demo = require("../../app/controllers/demo");
+
+module.exports = {
+    "/hello": demo.hello
+};
---
+++ src/biz/hello/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="hello"
+       class="hello-page">
+    <section class="header">HuangLiJun's Blog</section>
+    <section class="navigation">
+      <div class="entry"><a href="#">技术<br />思考</a></div>
+      <div class="entry"><a href="#">业务<br />思考</a></div>
+      <div class="entry"><a href="#">作品<br />案例</a></div>
+      <div class="entry"><a href="#">个人<br />简历</a></div>
+    </section>
+    <section class="content">
+      <div class="article">
+        <div class="time">2020-10-29 14:23:39</div>
+        <div class="title">微信小程序使用npm包的方法步骤</div>
+        <div class="descirption">主要介绍了微信小程序使用npm包的方法步骤，文中通过示例代码介绍的非常详细，对大家的学习或者工作具有一定的参考学习价值，需要的朋友们下面随着小编来一起学习学习吧……</div>
+      </div>
+      <div class="article">
+        <div class="time">2020-10-29 14:23:39</div>
+        <div class="title">微信小程序使用npm包的方法步骤</div>
+        <div class="descirption">主要介绍了微信小程序使用npm包的方法步骤，文中通过示例代码介绍的非常详细，对大家的学习或者工作具有一定的参考学习价值，需要的朋友们下面随着小编来一起学习学习吧……</div>
+      </div>
+      <div class="article">
+        <div class="time">2020-10-29 14:23:39</div>
+        <div class="title">微信小程序使用npm包的方法步骤</div>
+        <div class="descirption">主要介绍了微信小程序使用npm包的方法步骤，文中通过示例代码介绍的非常详细，对大家的学习或者工作具有一定的参考学习价值，需要的朋友们下面随着小编来一起学习学习吧……</div>
+      </div>
+      <div class="article">
+        <div class="time">2020-10-29 14:23:39</div>
+        <div class="title">微信小程序使用npm包的方法步骤</div>
+        <div class="descirption">主要介绍了微信小程序使用npm包的方法步骤，文中通过示例代码介绍的非常详细，对大家的学习或者工作具有一定的参考学习价值，需要的朋友们下面随着小编来一起学习学习吧……</div>
+      </div>
+      <div class="article">
+        <div class="time">2020-10-29 14:23:39</div>
+        <div class="title">微信小程序使用npm包的方法步骤</div>
+        <div class="descirption">主要介绍了微信小程序使用npm包的方法步骤，文中通过示例代码介绍的非常详细，对大家的学习或者工作具有一定的参考学习价值，需要的朋友们下面随着小编来一起学习学习吧……</div>
+      </div>
+      <div class="article">
+        <div class="time">2020-10-29 14:23:39</div>
+        <div class="title">微信小程序使用npm包的方法步骤</div>
+        <div class="descirption">主要介绍了微信小程序使用npm包的方法步骤，文中通过示例代码介绍的非常详细，对大家的学习或者工作具有一定的参考学习价值，需要的朋友们下面随着小编来一起学习学习吧……</div>
+      </div>
+    </section>
+  </div>
+
+</template>
+
+<style  scoped lang="less">
+@import "./index.less";
+</style>
+    
+<script>
+
+export default {
+  data () {
+    return {
+      message: "非常好"
+    }
+  },
+  components: {
+  },
+  computed: {
+
+  },
+  created () {
+  },
+  methods: {
+
+  }
+}
+</script>
+
